629664851fcca08725d8ac0c0a7c56bb2a306c44
frostzt <aidenfrostbite@gmail.com>
2025-07-17 14:15:01 +0530

    feat(serialization): entry can now be serialized

--- lib/entry.cpp
+++ lib/entry.cpp
@@ -2,4 +2,52 @@
 // Created by frostzt on 7/13/25.
 //
 
+#include <sstream>
+#include <iomanip>
+
 #include "entry.hpp"
+#include "utils/byte_utils.hpp"
+
+std::vector<std::byte> Entry::serialize() const {
+    std::vector<std::byte> byteV{};
+
+    // Write magic bytes
+    Utility::writeMagicBytes(byteV);
+
+    // Reserve 8 bytes for total size
+    for (int i = 0; i < 8; i++) byteV.push_back(static_cast<std::byte>(0)); // placeholder
+
+    Utility::writeString(byteV, this->tableName);
+    Utility::writeString(byteV, this->primaryKey_);
+    Utility::writeUint16(byteV, this->rowData_.size());
+
+    for (const auto &[key, value]: this->rowData_) {
+        Utility::writeString(byteV, key);
+        Utility::writeVariant(byteV, value);
+    }
+
+    byteV.push_back(static_cast<std::byte>(this->isTombstone_ ? 1 : 0));
+    Utility::writeUint64(byteV, this->timestamp_);
+
+    // Patch the header to update total size
+    const uint64_t totalSize = byteV.size() + 4; // +4 for checksum
+    Utility::patchUint64(byteV, 0, totalSize);
+
+    // Compute CRC32
+    const uint32_t checksum = Utility::computeCRC32(byteV, 0, byteV.size());
+    Utility::writeUint32(byteV, checksum);
+
+    return byteV;
+}
+
+std::string Entry::toHex() const {
+    const auto serialized = serialize();
+
+    std::ostringstream oss;
+    for (std::byte b: serialized) {
+        oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(b) << " ";
+    }
+
+    return oss.str();
+}
+
--- lib/entry.hpp
+++ lib/entry.hpp
@@ -2,6 +2,7 @@
 #define ENTRY_HPP
 
 #include <cstdint>
+#include <vector>
 #include <iostream>
 #include <variant>
 #include <unordered_map>
@@ -22,7 +23,12 @@ public:
 
     Entry(std::string table, std::string pk, Row data, const bool tombstone = false, const uint64_t ts = 0)
         : tableName(std::move(table)), primaryKey_(std::move(pk)), rowData_(std::move(data)),
-          isTombstone_(tombstone), timestamp_(ts) {}
+          isTombstone_(tombstone), timestamp_(ts) {
+    }
+
+    std::vector<std::byte> serialize() const;
+
+    std::string toHex() const;
 
     // AVL Tree needs to compare Entries based on primaryKey
     bool operator<(const Entry &other) const {
@@ -33,10 +39,11 @@ public:
         return primaryKey_ == other.primaryKey_;
     }
 
+
     friend std::ostream &operator<<(std::ostream &os, const Entry &e) {
         os << "[" << e.tableName << "] " << e.primaryKey_ << " {";
 
-        for (const auto &[k, v] : e.rowData_) {
+        for (const auto &[k, v]: e.rowData_) {
             os << k << ": ";
             std::visit([&](auto &&arg) {
                 os << arg;
