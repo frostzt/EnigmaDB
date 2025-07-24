158322eaf097d8315453ef7efe3cb679adf8458a
frostzt <aidenfrostbite@gmail.com>
2025-07-24 23:24:12 +0530

    feat(wal): added `getFileSize` and `filePath` impl

--- lib/wal/wal_manager.cpp
+++ lib/wal/wal_manager.cpp
@@ -1,5 +1,24 @@
 //
-// Created by aiden on 7/24/2025.
+// Created by frostzt on 7/24/2025.
 //
 
+#include <sys/stat.h>
+
 #include "wal_manager.hpp"
+
+namespace WAL {
+    size_t WALManager::getFileSize(const std::string &path) {
+        struct stat fileStat; /* POSIX ONLY */
+        if (stat(path.c_str(), &fileStat) < 0) {
+            return 0;
+        }
+
+        return fileStat.st_size;
+    }
+
+    std::string WALManager::filePath(const uint32_t fileId) const {
+        std::string filepath = walDir_ +  "/" + std::to_string(fileId) + ".wal";
+        return filepath;
+    }
+
+} // namespace WAL
--- lib/wal/wal_manager.hpp
+++ lib/wal/wal_manager.hpp
@@ -1,5 +1,5 @@
 //
-// Created by aiden on 7/24/2025.
+// Created by frostzt on 7/24/2025.
 //
 
 #ifndef WAL_MANAGER_HPP
@@ -10,50 +10,52 @@
 
 #include "lib/entry/entry.hpp"
 
-class WALManager {
-private:
-    std::string walDir_;
-    size_t maxFileSizeBytes_;
-    std::ofstream currentOut_;
-    size_t currentBytesWritten_;
-    uint32_t currentFileId_;
+namespace WAL {
+    class WALManager {
+    private:
+        std::string walDir_;
+        size_t maxFileSizeBytes_;
+        std::ofstream currentOut_;
+        size_t currentBytesWritten_;
+        uint32_t currentFileId_;
 
-    std::string filePath(uint32_t fileId);
+        std::string filePath(uint32_t fileId) const;
 
-    size_t getFileSize(const std::string &path);
+        static size_t getFileSize(const std::string &path);
 
-    uint32_t getLatestWALId();
+        uint32_t getLatestWALId();
 
-    bool init() {
-        currentFileId_ = getLatestWALId();
-        const std::string path = filePath(currentFileId_);
-        currentOut_.open(path, std::ios::out | std::ios::app | std::ios::binary);
-        if (!currentOut_.good()) return false;
+        bool init() {
+            currentFileId_ = getLatestWALId();
+            const std::string path = filePath(currentFileId_);
+            currentOut_.open(path, std::ios::out | std::ios::app | std::ios::binary);
+            if (!currentOut_.good()) return false;
 
-        currentBytesWritten_ = getFileSize(path);
-        return true;
-    }
-
-public:
-    static constexpr size_t operator"" _MB(const unsigned long long int mb) { return mb * 1024 * 1024; }
-
-    explicit WALManager(std::string walDir, size_t maxFileSizeBytes = 1_MB): walDir_(std::move(walDir)),
-                                                                             maxFileSizeBytes_(maxFileSizeBytes),
-                                                                             currentBytesWritten_(0),
-                                                                             currentFileId_(0) {
-        if (!init()) {
-            throw std::runtime_error("Failed to initialize WAL Manager!");
+            currentBytesWritten_ = getFileSize(path);
+            return true;
         }
+
+    public:
+        static constexpr size_t operator"" _MB(const unsigned long long int mb) { return mb * 1024 * 1024; }
+
+        explicit WALManager(std::string walDir, size_t maxFileSizeBytes = 1_MB): walDir_(std::move(walDir)),
+            maxFileSizeBytes_(maxFileSizeBytes),
+            currentBytesWritten_(0),
+            currentFileId_(0) {
+            if (!init()) {
+                throw std::runtime_error("Failed to initialize WAL Manager!");
+            }
+        };
+
+        bool append(const Entry &entry);
+
+        std::vector<Entry> loadAll();
+
+        void close();
+
+        void rotate();
     };
-
-    bool append(const Entry &entry);
-
-    std::vector<Entry> loadAll();
-
-    void close();
-
-    void rotate();
-};
+} // namespace WAL
 
 
 #endif //WAL_MANAGER_HPP
