337d7a680c7b63e8820f6b67460dfb4df6969cb7
frostzt <aidenfrostbite@gmail.com>
2025-08-03 14:59:40 +0530

    chore: update type registry and descriptor includes

--- lib/datatypes/type_registry.hpp
+++ lib/datatypes/type_registry.hpp
@@ -2,10 +2,12 @@
 // Created by frostzt on 8/2/2025.
 //
 
+#pragma once
 #ifndef TYPE_REGISTRY_HPP
 #define TYPE_REGISTRY_HPP
 
 #include "type_descriptor.hpp"
+#include "field_type.hpp"
 
 namespace core::datatypes {
     class TypeRegistry {
