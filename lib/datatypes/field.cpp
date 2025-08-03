06ba2ebb583c69b5086e96e8088903126a1efcf4
frostzt <aidenfrostbite@gmail.com>
2025-08-03 14:59:00 +0530

    feat: update field to include serialize/deserialize

--- lib/datatypes/field.cpp
+++ lib/datatypes/field.cpp
@@ -9,7 +9,7 @@ namespace core::datatypes {
         // Note: My IDE is going all out on this because `actual` is only used to decay and get
         //       a type; it is asking to overload all types but it is not required as we can get
         //       the type by decaying bruh :|
-        return std::visit([&](auto&& actual) -> bool {
+        return std::visit([&](auto &&actual) -> bool {
             using T = std::decay_t<decltype(actual)>;
 
             switch (type) {
@@ -19,16 +19,41 @@ namespace core::datatypes {
                 case FieldType::String: return std::is_same_v<T, std::string>;
                 case FieldType::Bool: return std::is_same_v<T, bool>;
                 case FieldType::Timestamp: return std::is_same_v<T, std::chrono::system_clock::time_point>;
-                case FieldType::Binary: return std::is_same_v<T, std::vector<uint8_t>>;
+                case FieldType::Binary: return std::is_same_v<T, std::vector<uint8_t> >;
                 case FieldType::Null: return std::is_same_v<T, std::monostate>;
                 case FieldType::Custom: return true; // validated by descriptor
+                case FieldType::UUID: return std::is_same_v<T, UUID>;
                 default: return false;
             }
         }, val);
     }
 
-
     bool Field::isNull() const {
         return std::holds_alternative<std::monostate>(this->value_);
+    }
+
+    std::string Field::toString() const {
+        return this->getDescriptor()->toString(this->value_);
+    }
+
+    std::vector<std::byte> Field::serialize() const {
+        std::vector<std::byte> out;
+        out.push_back(static_cast<std::byte>(this->type_));
+
+        const auto *desc = this->getDescriptor();
+        std::vector<std::byte> data = desc->serialize(this->value_);
+        out.insert(out.end(), data.begin(), data.end());
+
+        return out;
+    }
+
+    Field Field::deserialize(const std::byte *data, const size_t len) {
+        if (len < 1) throw std::runtime_error("Invalid field length");
+
+        const auto type = static_cast<FieldType>(data[0]);
+        const auto *desc = TypeRegistry::get(type);
+        const FieldValue val = desc->deserialize(data + 1, len - 1);
+
+        return Field{val, type, nullptr};
     }
 } // namespace core::datatypes
